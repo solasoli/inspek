cb2934431285c1e6a9c8552d29fef2d6fd8a8371
thekuchel <adamalamsyah95@gmail.com>
2020-01-20 01:48:43 +0700

    rename kode bank to rka

--- resources/views/Mst/rka_add_temp-form.blade.php
+++ resources/views/Mst/rka_add_temp-form.blade.php
@@ -3,12 +3,12 @@
 <div class="br-pageheader pd-y-15 pd-l-20">
   <nav class="breadcrumb pd-0 mg-0 tx-12">
     <a class="breadcrumb-item" href="/">Dashboard</a>
-    <a class="breadcrumb-item Active" href="#">Upload Kode Bank</a>
+    <a class="breadcrumb-item Active" href="#">Upload RKA</a>
   </nav>
 </div>
 
 <div class="pd-x-20 pd-sm-x-30 pd-t-20 pd-sm-t-30">
-  <h4 class="tx-gray-800 mg-b-5">Upload Kode Bank</h4>
+  <h4 class="tx-gray-800 mg-b-5">Upload RKA</h4>
 </div>
 
 <div class="br-pagebody">
