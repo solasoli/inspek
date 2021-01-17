2286b0d83ebc80192e36c2ba02242e1955146bcd
thekuchel <adamalamsyah95@gmail.com>
2021-01-18 00:46:33 +0700

    label jadi pilih sasaran

--- resources/views/pkpt/partial/surat_perintah_base.blade.php
+++ resources/views/pkpt/partial/surat_perintah_base.blade.php
@@ -15,7 +15,7 @@
     <div class="card-body">
     <div class="form-group row" style="margin-bottom: 10px">
         <label class="form-control-label col-md-3 col-sm-3 col-xs-12">
-        Pilih Kegiatan
+        Pilih Sasaran
         </label>
         <div class="col-md-6 col-sm-6 col-xs-12">
         <select name='program_kerja' class="form-control select2 kegiatan" id='program_kerja'>
